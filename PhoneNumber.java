d06a34ca520c259355c5616628706e9627067fdb
seoul-chan <kkchan9210@gmail.com>
2021-01-13 15:18:38 +0900

    프로그래머스 Level 2 - 전화번호 목록
    완료 코드 커밋 (startsWith 이용)

--- PhoneNumber.java
+++ PhoneNumber.java
@@ -1,15 +1,13 @@
 package CodingTest;
 
-import java.util.Arrays;
-
 public class PhoneNumber {
     static boolean solution(String[] phone_book){
         boolean answer = true;
-        Arrays.sort(phone_book);
 
-        for (int i=1;i<phone_book.length;i++) {     
-            if(phone_book[i].indexOf(phone_book[0])==0){
-                answer = false;
+        for (int i=0;i<phone_book.length;i++) {
+            for(int j=i+1;j<phone_book.length;j++) {
+                if(phone_book[i].startsWith(phone_book[j])) return false;
+                if(phone_book[j].startsWith(phone_book[i])) return false;
             }
         }
         return answer;
