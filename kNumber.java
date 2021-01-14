b185e5d800ecade7c199cfbcd1a78e6f95f3dd1f
seoul-chan <kkchan9210@gmail.com>
2021-01-14 23:58:34 +0900

    프로그래머스 Level 1 - K번째수
    완료 코드 커밋

---
+++ kNumber.java
@@ -0,0 +1,28 @@
+package CodingTest;
+
+import java.util.Arrays;
+
+class Solutions {
+    public int[] solutions(int[] array, int[][] commands){
+        int[] answer = new int[commands.length];
+
+        for(int i=0;i<commands.length;i++){
+            int[] temp = Arrays.copyOfRange(array, commands[i][0]-1, commands[i][1]);
+            Arrays.sort(temp);
+            answer[i] = temp[commands[i][2]-1];
+        }
+        return answer;
+    }
+}
+
+
+public class kNumber {
+    public static void main(String[] args) {
+        Solutions returns = new Solutions();
+        int[] array = {1,5,2,6,3,7,4};
+        int[][] commands = {{2,5,3},{4,4,1},{1,7,3}};
+
+        System.out.println(Arrays.toString(returns.solutions(array, commands)));
+
+    }
+}
