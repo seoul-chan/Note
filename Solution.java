31a79fa54fa4abbad43a5e657618290205d68f71
seoul-chan <kkchan9210@gmail.com>
2021-01-22 17:51:17 +0900

    프로그래머스 Level 1 - 두 정수 사이의 합
    완료 커밋

--- Solution.java
+++ Solution.java
@@ -1,31 +1,22 @@
 package CodingTest;
 
-import java.util.Arrays;
-
 class Solution {
-    public int[] solution(int[] arr, int divisor) {
-        int count = 0,two=0;
-        for(int i=0;i<arr.length;i++) {
-        	if(arr[i]%divisor==0) count++;
-        }
+    public long solution(int a, int b) {
+        int max=(a>b)?a:b,
+        	min=(a<b)?a:b;
+        long answer=0;
         
-        if(count==0) {
-        	int[] answer = {-1};
-        	return answer;
+        if(a==b) return a;
+        for(int i=min;i<=max;i++) {
+        	answer+=i;
         }
-        
-        int[] answer = new int[count];
-        for(int i=0;i<arr.length;i++) {
-        	if(arr[i]%divisor==0) answer[two++]=arr[i];
-        }
-        Arrays.sort(answer);
         return answer;
     }
     
     public static void main(String[] args) {
     	Solution sol = new Solution();
-    	int[] arr  = {2,36,1,3};
-    	int divisor = 1;
-    	System.out.println(Arrays.toString(sol.solution(arr,divisor)));
+    	int a = 5;
+    	int b = 6;
+    	System.out.println(sol.solution(a, b));
     }
 }
