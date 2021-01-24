c8aca44c8f8028d0e085336957f12cf6f81b42c3
seoul-chan <kkchan9210@gmail.com>
2021-01-24 23:13:26 +0900

    프로그래머스 Level 1 - 같은 숫자는 싫어
    완료 코드 커밋(ArrayList)

--- Solution.java
+++ Solution.java
@@ -1,28 +1,29 @@
 package codingTest;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 //프로그래머스 Level 1 - 같은 숫자는 싫어
 public class Solution {
     public int[] solution(int []arr) {
-        int prev=1;
+        ArrayList<Integer> arrlist = new ArrayList<Integer>();
+        int prev=10;
         
-        for(int i=0;i<arr.length-1;i++) {
-        	if(arr[i]!=arr[i+1]) prev++; 
+        for(int i=0;i<arr.length;i++) {
+        	if(prev!=arr[i]) arrlist.add(arr[i]);
+        	prev = arr[i];
         }
         
-        int[] answer = new int[prev];
-        prev=1;
-        answer[0]=arr[0];
-        for(int i=0;i<arr.length-1;i++) {
-        	if(arr[i]!=arr[i+1]) answer[prev++]=arr[i+1];
+        int[] answer = new int[arrlist.size()];
+        for(int i=0;i<answer.length;i++) {
+        	answer[i]=arrlist.get(i);
         }
         return answer;
     }
     
     public static void main(String[] args) {
     	Solution sol = new Solution();
-    	int[] arr = {5000,56222,56222,8888,8999};
+    	int[] arr = {1,1,3,3,0,1,1};
     	
     	System.out.println(Arrays.toString(sol.solution(arr)));
     }
