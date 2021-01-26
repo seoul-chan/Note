00b5c4671d1c4cad7f30d5450103b1b0ef68e429
seoul-chan <kkchan9210@gmail.com>
2021-01-26 20:36:57 +0900

    프로그래머스 Level 1 - 문자열 내 마음대로 정렬하기

--- Solution.java
+++ Solution.java
@@ -1,30 +1,31 @@
 package codingTest;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 
-//프로그래머스 Level 1 - 같은 숫자는 싫어
-public class Solution {
-    public int[] solution(int []arr) {
-        ArrayList<Integer> arrlist = new ArrayList<Integer>();
-        int prev=10;
-        
-        for(int num : arr) {
-        	if(prev!=num) arrlist.add(num);
-        	prev = num;
-        }
-        
-        int[] answer = new int[arrlist.size()];
-        for(int i=0;i<answer.length;i++) {
-        	answer[i]=arrlist.get(i);
-        }
-        return answer;
+//프로그래머스 Level 1 - 문자열 내 마음대로 정렬하기
+class Solution {
+    public String[] solution(String[] strings, int n) {
+    	Arrays.sort(strings);
+    	
+    	for(int i=0;i<strings.length-1;i++) {
+    		for(int j=1;j<strings.length-i;j++) {
+    			if(strings[j-1].charAt(n)>strings[j].charAt(n)) {
+	    			String temp = strings[j];
+	    			strings[j] = strings[j-1];
+	    			strings[j-1] = temp;
+        		}
+    		}
+    	}
+        return strings;
     }
     
     public static void main(String[] args) {
     	Solution sol = new Solution();
-    	int[] arr = {1,1,3,3,0,1,1};
+    	String[] answer = {"sun","bed","car"};
     	
-    	System.out.println(Arrays.toString(sol.solution(arr)));
+    	System.out.println(Arrays.toString(sol.solution(answer,1)));
     }
 }
+
+//1. 입력 받은 문자열에서 n번째 글자 출력
+//2. 
