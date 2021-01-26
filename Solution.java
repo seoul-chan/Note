b0f2e17c9fb6d99ed46a0accace90068d3e509f2
seoul-chan <kkchan9210@gmail.com>
2021-01-26 22:39:09 +0900

    프로그래머스 Level 1 - 문자열 내 마음대로 정렬하기[compareTo()이용]

--- Solution.java
+++ Solution.java
@@ -5,14 +5,20 @@ import java.util.Arrays;
 //프로그래머스 Level 1 - 문자열 내 마음대로 정렬하기
 class Solution {
     public String[] solution(String[] strings, int n) {
-    	Arrays.sort(strings);
     	
-    	for(int i=0;i<strings.length-1;i++) {
-    		for(int j=1;j<strings.length-i;j++) {
-    			if(strings[j-1].charAt(n)>strings[j].charAt(n)) {
-	    			String temp = strings[j];
-	    			strings[j] = strings[j-1];
-	    			strings[j-1] = temp;
+    	for(int i=0;i<strings.length;i++) {
+    		for(int j=i+1;j<strings.length;j++) {
+    			if(strings[i].charAt(n)>strings[j].charAt(n)) {
+	    			String temp = strings[i];
+	    			strings[i] = strings[j];
+	    			strings[j] = temp;
+	    			
+        		} else if(strings[i].charAt(n)==strings[j].charAt(n)) {
+        			if(strings[i].compareTo(strings[j])>0) {
+        				String temp = strings[i];
+    	    			strings[i] = strings[j];
+    	    			strings[j] = temp;
+        			}
         		}
     		}
     	}
@@ -26,6 +32,3 @@ class Solution {
     	System.out.println(Arrays.toString(sol.solution(answer,1)));
     }
 }
-
-//1. 입력 받은 문자열에서 n번째 글자 출력
-//2. 
