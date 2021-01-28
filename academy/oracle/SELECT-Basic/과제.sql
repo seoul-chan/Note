14d052810b3add65a2b95f352a221dcc99895be4
seoul-chan <kkchan9210@gmail.com>
2021-01-28 12:00:00 +0900

    SELECT Basic 과제 완료
    SELECT Basic 과제 완료

---
+++ academy/oracle/SELECT-Basic/과제.sql
@@ -0,0 +1,91 @@
+1. JOB테이블에서 JOB_NAME의 정보만 출력되도록 하시오
+SELECT JOB_NAME FROM JOB;
+
+2. DEPARTMENT테이블의 내용 전체를 출력하는 SELECT문을 작성하시오
+SELECT * FROM DEPARTMENT;
+
+3. EMPLOYEE 테이블에서 이름(EMP_NAME),이메일(EMAIL),전화번호(PHONE),고용일(HIRE_DATE)만 출력하시오
+SELECT EMP_NAME, EMAIL, PHONE, HIRE_DATE FROM EMPLOYEE ;
+
+4. EMPLOYEE 테이블에서 고용일(HIRE_DATE) 이름(EMP_NAME),월급(SALARY)를 출력하시오
+SELECT HIRE_DATE, EMP_NAME, SALARY FROM EMPLOYEE;
+
+5. EMPLOYEE 테이블에서 월급(SALARY)이 2,500,000원이상인 사람의 EMP_NAME 과 SAL_LEVEL을 출력하시오 
+SELECT EMP_NAME, SAL_LEVEL FROM EMPLOYEE WHERE SALARY>=2500000;
+
+6. EMPLOYEE 테이블에서 월급(SALARY)이 350만원 이상이면서 
+    JOB_CODE가 'J3' 인 사람의 이름(EMP_NAME)과 전화번호(PHONE)를 출력하시오
+SELECT EMP_NAME, SAL_LEVEL FROM EMPLOYEE WHERE SALARY>=2500000;
+
+1. EMPLOYEE 테이블에서 이름, 연봉, 총수령액(보너스포함), 실수령액(총 수령액-(월급*세금 3%))가 출력되도록 하시오
+  BONUS가 NULL인 사원은 NULL로 출력
+SELECT EMP_NAME AS 이름, SALARY*12 AS 연봉, 
+(SALARY*12)*(1+(BONUS/100)) AS 총수령액, ((SALARY*12)*(1+(BONUS/100))-(SALARY*(1-(3/100)))) AS 실수령액
+FROM EMPLOYEE;
+WHERE SALARY>=2500000;
+
+2. EMPLOYEE 테이블에서 이름, 근무 일수(입사한지 몇일인가)를 출력해보시오.
+
+SELECT EMP_NAME AS 이름, SYSDATE-HIRE_DATE AS 근무일수
+FROM EMPLOYEE;
+
+3. EMPLOYEE 테이블에서 20년 이상 근속자의 이름,월급,보너스율를 출력하시오
+SELECT EMP_NAME AS 이름, SALARY AS 월급, BONUS AS 보너스율
+FROM EMPLOYEE
+WHERE SYSDATE-HIRE_DATE >=7300;
+
+부서 코드가 D6 이고 급여를 2000000보다 많이 받는 지원의 이름, 부서코드,급여 조회
+SELECT EMP_NAME AS 이름, DEPT_CODE AS 부서코드, SALARY AS 급여
+FROM EMPLOYEE
+WHERE DEPT_CODE='D6' AND SALARY>=2000000;
+
+부서코드가 D6가 아닌 사원조회
+SELECT *
+FROM EMPLOYEE
+WHERE NOT DEPT_CODE='D6';
+
+직급코드가 J1이 아닌 사원들의 월급등급(sal_level)을 중복없이 출력하세요.
+SELECT DISTINCT SAL_LEVEL
+FROM EMPLOYEE
+WHERE NOT DEPT_CODE='J1';
+
+EMPLOYEE 테이블에서 고용일이 90/01/01 ~ 01/01/01 인 사원의 전체 내용을 출력하시오
+SELECT *
+FROM EMPLOYEE
+WHERE HIRE_DATE >'90/01/01' AND HIRE_DATE <'01/01/01';
+
+이름 가운데 글자가 '옹'인 사원 조회(이름은 모두 3글자임)
+SELECT *
+FROM EMPLOYEE
+WHERE EMP_NAME LIKE '_옹_';
+
+EMAIL _앞이 4글자인 사원의 이름 이메일을 조회하세요
+SELECT EMP_NAME, EMAIL
+FROM EMPLOYEE
+WHERE EMAIL LIKE '____\_%' ESCAPE '\';
+
+1. EMPLOYEE 테이블에서 이름 끝이 연으로 끝나는 사원의 이름을 출력하시오
+SELECT *
+FROM EMPLOYEE
+WHERE EMP_NAME LIKE '%연';
+
+2. EMPLOYEE 테이블에서 전화번호 처음 3자리가 010이 아닌 사원의 이름, 전화번호를 출력하시오
+SELECT EMP_NAME, PHONE
+FROM EMPLOYEE
+WHERE PHONE NOT LIKE '010%';
+
+3. EMPLOYEE 테이블에서 메일주소 '_'의 앞이 4자이면서, DEPT_CODE가 D9 또는 D6이고
+  고용일이 90/01/01 ~ 00/12/01이면서, 월급이 270만원인 사원의 전체 정보를 출력하시오 
+SELECT *
+FROM EMPLOYEE
+WHERE EMAIL LIKE '____\_%' ESCAPE '\' AND DEPT_CODE='D9' OR DEPT_CODE='D6'
+AND HIRE_DATE>'90/01/01' AND HIRE_DATE<'00/12/01';  
+  부서 배치를 받지 않았지만 보너스를 지급하는 직원 조회 
+SELECT *
+FROM EMPLOYEE
+WHERE DEPT_CODE IS NULL AND BONUS IS NOT NULL;
+  
+  관리자도 없고 부서 배치도 받지 않은 직원 이름 조회
+SELECT *
+FROM EMPLOYEE
+WHERE MANAGER_ID IS NULL AND DEPT_CODE IN NULL;
