2f5ef1124d92570c67c180a811fc23e785e41367
seoul-chan <kkchan9210@gmail.com>
2021-01-29 22:05:46 +0900

    프로그래머스 Level 1 - 약수의 합

--- Solution.java
+++ Solution.java
@@ -1,30 +1,18 @@
 package codingTest;
 
-////프로그래머스 Level 1 - 문자열 내 p와 y의 개수
+//프로그래머스 Level 1 - 약수의 합
 class Solution {
-	public boolean solution(String s) {
-		s = s.toLowerCase();
-		int count=0;
-		
-		for(int i=0;i<s.length();i++) {
-			// 방법 1
-//			if(s.charAt(i)=='p') count++;
-//			if(s.charAt(i)=='y') count--;
-			
-			//방법 2
-			switch(s.charAt(i)) {
-				case 'p' : count++; break;
-				case 'y' : count--; break;
-			}
-		}
-		
-		if(count!=0) return false;
-		
-		return true;
-	}
-	
-	public static void main(String[] args) {
-		Solution sol = new Solution();
-		System.out.println(sol.solution("Pyy"));
-	}
+    public int solution(int n) {
+    	int answer = 0;
+    	for(int i=1;i<=n;i++) {
+    		answer=(n%i==0)?answer+=i:answer;
+    	}
+        
+        return answer;
+    }
+    
+    public static void main(String[] args) {
+    	Solution sol = new Solution();
+    	System.out.println(sol.solution(5));
+    }
 }
