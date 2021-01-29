903dcd03400b15b6aa703ae0e0c45ee3d5c66c78
seoul-chan <kkchan9210@gmail.com>
2021-01-29 19:50:05 +0900

    프로그래머스 Level 1 - 문자열 내 p와 y의 개수

--- Solution.java
+++ Solution.java
@@ -1,24 +1,23 @@
 package codingTest;
 
-// 프로그래머스 Level 1 - 문자열 다루기 기본
+
+//프로그래머스 Level 1 - 문자열 내 p와 y의 개수
 class Solution {
-    public boolean solution(String s) {
-        boolean answer = true;
-        
-        if(s.length()!=4 && s.length()!=6) return false;
-        	
-        for(int i=0;i<s.length();i++) {
-        	if(s.charAt(i)<48 || s.charAt(i)>57)
-        		answer = false;
-        }       
-        return answer;
-    }
-    
-    public static void main(String[] args) {
-    	Solution sol = new Solution();
-    	String s  = "99b9";
-    	System.out.println(sol.solution(s));
-    }
+	public boolean solution(String s) {
+		int pcount=0, ycount=0;
+		
+		for(int i=0;i<s.length();i++) {
+			if(s.charAt(i)=='p' || s.charAt(i)=='P') pcount++;
+			if(s.charAt(i)=='y' || s.charAt(i)=='Y') ycount++;
+		}
+		
+		if(pcount!=ycount) return false;
+		
+		return true;
+	}
+	
+	public static void main(String[] args) {
+		Solution sol = new Solution();
+		System.out.println(sol.solution("Pyy"));
+	}
 }
-
-
