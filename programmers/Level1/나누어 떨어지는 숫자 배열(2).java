c2af31e0720884a4798a0387bac4569821312fc6
seoul-chan <76949587+seoul-chan@users.noreply.github.com>
2021-01-30 09:55:06 +0900

    나누어 떨어지는 숫자 배열(2)
    문제사이트 : https://programmers.co.kr/learn/courses/30/lessons/12910

---
+++ programmers/Level1/나누어 떨어지는 숫자 배열(2).java
@@ -0,0 +1,31 @@
+package codingTest;
+
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] arr, int divisor) {
+        int count = 0,two=0;
+        for(int i=0;i<arr.length;i++) {
+        	if(arr[i]%divisor==0) count++;
+        }
+        
+        if(count==0) {
+        	int[] answer = {-1};
+        	return answer;
+        }
+        
+        int[] answer = new int[count];
+        for(int i=0;i<arr.length;i++) {
+        	if(arr[i]%divisor==0) answer[two++]=arr[i];
+        }
+        Arrays.sort(answer);
+        return answer;
+    }
+    
+    public static void main(String[] args) {
+    	Solution sol = new Solution();
+    	int[] arr  = {2,36,1,3};
+    	int divisor = 1;
+    	System.out.println(Arrays.toString(sol.solution(arr,divisor)));
+    }
+}
