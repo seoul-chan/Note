53995e8b94a4fe40b87c7848d6738172d75d7448
seoul-chan <76949587+seoul-chan@users.noreply.github.com>
2021-01-31 17:21:38 +0900

    문자열 내 마음대로 정렬하기
    문제 사이트 : https://programmers.co.kr/learn/courses/30/lessons/12915

---
+++ programmers/Level1/문자열 내 마음대로 정렬하기.java
@@ -0,0 +1,27 @@
+package codingTest;
+
+import java.util.Arrays;
+
+class Solution {
+    public String[] solution(String[] strings, int n) {
+    	Arrays.sort(strings);
+    	
+    	for(int i=0;i<strings.length-1;i++) {
+    		for(int j=1;j<strings.length-i;j++) {
+    			if(strings[j-1].charAt(n)>strings[j].charAt(n)) {
+	    			String temp = strings[j];
+	    			strings[j] = strings[j-1];
+	    			strings[j-1] = temp;
+        		}
+    		}
+    	}
+        return strings;
+    }
+    
+    public static void main(String[] args) {
+    	Solution sol = new Solution();
+    	String[] answer = {"sun","bed","car"};
+    	
+    	System.out.println(Arrays.toString(sol.solution(answer,1)));
+    }
+}
