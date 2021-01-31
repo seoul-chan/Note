3b94b24c59e91dd334c5ffac900a07caac3db2e0
seoul-chan <76949587+seoul-chan@users.noreply.github.com>
2021-01-31 17:19:11 +0900

    같은 숫자는 싫어(2)
    문제 사이트 : https://programmers.co.kr/learn/courses/30/lessons/12906

---
+++ programmers/Level1/같은 숫자는 싫어(2).java
@@ -0,0 +1,29 @@
+package codingTest;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Solution {
+    public int[] solution(int []arr) {
+        ArrayList<Integer> arrlist = new ArrayList<Integer>();
+        int prev=10;
+        
+        for(int i=0;i<arr.length;i++) {
+        	if(prev!=arr[i]) arrlist.add(arr[i]);
+        	prev = arr[i];
+        }
+        
+        int[] answer = new int[arrlist.size()];
+        for(int i=0;i<answer.length;i++) {
+        	answer[i]=arrlist.get(i);
+        }
+        return answer;
+    }
+    
+    public static void main(String[] args) {
+    	Solution sol = new Solution();
+    	int[] arr = {1,1,3,3,0,1,1};
+    	
+    	System.out.println(Arrays.toString(sol.solution(arr)));
+    }
+}
