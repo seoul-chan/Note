cef90b9ee2fb41e006f265a328bcd29266c2204d
seoul-chan <76949587+seoul-chan@users.noreply.github.com>
2021-02-01 21:29:23 +0900

    문자열 다루기 기본
    문제 사이트 : https://programmers.co.kr/learn/courses/30/lessons/12918

---
+++ programmers/Level1/문자열 다루기 기본.java
@@ -0,0 +1,21 @@
+package codingTest;
+
+class Solution {
+	public boolean solution(String s) {
+	   boolean answer = true;
+	   
+	   if(s.length()!=4 && s.length()!=6) return false;
+	   	
+	   for(int i=0;i<s.length();i++) {
+	   	if(s.charAt(i)<48 || s.charAt(i)>57)
+	   		answer = false;
+	   }       
+	   return answer;
+	}
+	
+	public static void main(String[] args) {
+		Solution sol = new Solution();
+		String s  = "99b9";
+		System.out.println(sol.solution(s));
+	}
+}
