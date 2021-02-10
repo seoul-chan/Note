da913d21a7e398abc106009070339c8cf5e986ef
seoul-chan <76949587+seoul-chan@users.noreply.github.com>
2021-02-10 23:23:41 +0900

    숫자야구게임
    숫자야구게임 커밋2

--- academy/BaseballGame.java
+++ academy/BaseballGame.java
@@ -1,6 +1,5 @@
 package com.semi.baseball;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class BaseballGame {
@@ -14,8 +13,8 @@ public class BaseballGame {
 			for(int j=0;j<i;j++) {
 				if(random[i].equals(random[j])) i--;
 			}
+			System.out.print(random[i]+" ");
 		}
-		System.out.println(random.toString());
 
 		while(true) {
 			int strike=0,ball=0;
@@ -23,42 +22,24 @@ public class BaseballGame {
 			System.out.println("==== 숫자 야구게임 ====");	//사용자 입력
 			System.out.print("숫자를 입력해주세요(4자리) : ");
 			String[] input = sc.next().split("");
-			String[] copyinput = Arrays.copyOf(input, input.length);
 			
+			//스트라이크
 			for(int i=0;i<4;i++) {
-				for(int j=0;j<4;j++) {
-					if(input[i]==random[i]) {
-						strike++;
-						continue;
-					} else {
-						ball++;
-					}
+				if(input[i].equals(random[i])) {
+					strike++;
+					input[i]="0";
 				}
 			}
+			
+			//볼
+			for(int i=0;i<4;i++) {
+				if(random[i].contains(input[i])) {
+					ball++;
+				}
+			}
+			
 			System.out.println("strike : "+strike+", ball : "+ball+"\n");
 			if(strike==4) break;
 		}
-//		while(true) {
-//			int strike=0,ball=0;
-//			
-//			System.out.println("==== 숫자 야구게임 ====");	//사용자 입력
-//			System.out.print("숫자를 입력해주세요(4자리) : ");
-//			String input = sc.next();
-//			for(int i=0;i<4;i++) {
-//				for(int j=0;j<4;j++) {
-//					if(input.charAt(i)==random.charAt(i)) {
-//						strike++;
-//						continue;
-//					}
-//					
-//					
-//				}
-//			}
-//			System.out.println("strike : "+strike+", ball : "+ball+"\n");
-//			if(strike==4) {
-//				System.out.println("다시 하시겠습니까? (Y/N)");
-//				if(sc.next().equals("N")) break;
-//			}
-//		}
 	}
 }
