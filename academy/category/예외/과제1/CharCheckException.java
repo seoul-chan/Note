8e2e07945bdefb045f1d9dc0516d1ff32ec71160
seoul-chan <76949587+seoul-chan@users.noreply.github.com>
2021-02-19 14:46:30 +0900

    예외 출력 과제 1(완료)
    예외 출력 과제 1(완료)

---
+++ academy/category/예외/과제1/CharCheckException.java
@@ -0,0 +1,10 @@
+package com.kh.pratice.charCheck.exception;
+
+public class CharCheckException extends Exception{
+	
+	public CharCheckException() {}
+	
+	public CharCheckException(String msg) {
+		super(msg);
+	}
+}
---
+++ academy/category/예외/과제1/Run.java
@@ -0,0 +1,9 @@
+package com.kh.pratice.charCheck.run;
+
+import com.kh.pratice.charCheck.view.CharacterMenu;
+
+public class Run {
+	public static void main(String[] args) {
+		new CharacterMenu().menu();
+	}
+}
