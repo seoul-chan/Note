a1a4f44f204b11ecd56906783f53c197351e2597
seoul-chan <kkchan9210@gmail.com>
2021-03-10 23:49:24 +0900

    codeup 기초 100제 시작
    codeup 기초 100제 시작

---
+++ codeup/Main.java
@@ -0,0 +1,36 @@
+package com.test;
+
+public class Main {
+	public static void main(String[] args) {
+	
+	}
+}
+
+/*
+	CodeUp-1008
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.printf("\u250c\u252c\u2510\n"
+				+ "\u251c\u253c\u2524\n"
+				+ "\u2514\u2534\u2518");		
+	}
+
+	CodeUp-1020
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println(sc.next().replace("-", ""));		
+	}
+
+
+ 	CodeUp-1019
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String[] days = sc.next().split("\\.");
+		int[] day = new int[days.length];
+		for(int i=0;i<days.length;i++)
+			day[i] = Integer.valueOf(days[i]);
+				
+		System.out.printf("%04d.%02d.%02d",day[0],day[1],day[2]);
+	}
+*/
+
