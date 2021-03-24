8e2bb90f4cbc2b5e8647e46e9ab1a01a20a38a4b
seoul-chan <kkchan9210@gmail.com>
2021-03-25 00:52:22 +0900

    달팽이는 올라가고 싶다
    문제 사이트 : https://www.acmicpc.net/problem/2869

--- baekjoon/기본수학1/[2869]달팽이는 올라가고 싶다.java
+++ baekjoon/기본수학1/[2869]달팽이는 올라가고 싶다.java
@@ -1,20 +1,30 @@
 package com.test;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
 
 public class Main {
 	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		int day=sc.nextInt(),night=sc.nextInt(),tree=sc.nextInt();
-		int move=0,count=0;
-		
-		while(true) {
-			count++;
-			if((move+=day)>=tree) break;
-			move-=night;
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		try {
+			StringTokenizer st = new StringTokenizer(br.readLine()," ");
+			int up=Integer.valueOf(st.nextToken());
+			int down=Integer.valueOf(st.nextToken());
+			int length=Integer.valueOf(st.nextToken());
+			
+			int count=(length-down)/(up-down);
+			
+			if((length-down)%(up-down)!=0) count++;
+			System.out.println(count);
+		}catch(IOException e) {
+			e.printStackTrace();
 		}
-		System.out.println(count);
-		System.out.println(20%7);
 	}
 }
 
