b2d7eaaa03bcc17038340f79394330af2e043662
seoul-chan <kkchan9210@gmail.com>
2021-03-27 11:21:33 +0900

    한조서열정리하고옴
    문제 사이트 : https://www.acmicpc.net/problem/14659

--- baekjoon/그리디/[14659]한조서열정리하고옴.java
+++ baekjoon/그리디/[14659]한조서열정리하고옴.java
@@ -1,26 +1,30 @@
 package com.test;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
 	//14659번 - 한조서열정리하고옴
-    public static void main(String[] args) {
-    	Scanner sc=new Scanner(System.in);
-    	int mount=sc.nextInt();
-    	int[] arr=new int[mount];
+    public static void main(String[] args) throws IOException {
+    	BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+    	
+    	int peaks=Integer.parseInt(br.readLine());
+    	String[] heights = br.readLine().split(" ");
+    	int cur_height=Integer.parseInt(heights[0]);
     	int count=0,max=0;
     	
-    	for(int i=0;i<mount;i++) {
-    		arr[i]=sc.nextInt();
-    	}
-    	
-    	for(int i=0;i<mount;i++) {
-    		for(int j=i+1;j<mount;j++) {
-    			if(arr[i]>arr[j]) count++;
-    			else break;
+    	for(int i=1;i<peaks;i++) {
+    		int height=Integer.parseInt(heights[i]);
+    		
+    		if(cur_height>height) {
+    			count++;
+    			if(count>max)
+    				max=count;
+    		}else {
+    			cur_height=height;
+    			count=0;
     		}
-    		if(count>max) max=count;
-    		count=0;
     	}
     	System.out.println(max);
     }	
