7021bf19ff4848b7d2704eff0ffeab83d252d2f6
seoul-chan <kkchan9210@gmail.com>
2021-03-28 13:58:43 +0900

    숫자의 개수
    문제 사이트 : https://www.acmicpc.net/problem/2577

--- baekjoon/[2577]숫자의 개수.java
+++ baekjoon/[2577]숫자의 개수.java
@@ -1,16 +1,24 @@
-package coding;
+package com.test;
 
-public class Test {	
-	public static void main(String[] args) {
-		int a=150,b=266,c=999;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	//2577번 - 숫자의 개수
+	public static void main(String[] args) throws IOException{
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		int a=Integer.parseInt(br.readLine());
+		int b=Integer.parseInt(br.readLine());
+		int c=Integer.parseInt(br.readLine());
 		int count=0;
-		String s = String.valueOf((a*b*c));
-		System.out.println(s);
+		String sum = String.valueOf((a*b*c));
+		
 		for(int i=0;i<10;i++) {
-			for(int j=0;j<s.length();j++) {
-				if((Integer.valueOf(s.charAt(j))-'0')==i) count++;
+			for(int j=0;j<sum.length();j++) {
+				if((Integer.valueOf(sum.charAt(j))-'0')==i) count++;
 			}
-			System.out.print(count);
+			System.out.println(count);
 			count=0;
 		}
 	} // main
