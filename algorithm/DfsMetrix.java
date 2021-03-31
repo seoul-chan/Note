aa608e56a8a1d4931e477c6dbfdd7a620a719034
seoul-chan <kkchan9210@gmail.com>
2021-03-31 17:15:29 +0900

    DFS 인접 행렬방식 구현(재귀) - 주석 추가
    DFS 인접 행렬방식 구현(재귀) - 주석 추가

--- algorithm/DfsMetrix.java
+++ algorithm/DfsMetrix.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class DfsMetrix {
 
-	static int edge;	// 노드간 연결하는 간선의 수
+	static int edge;	//노드간 연결하는 간선의 수
 	static int vertex;	//노드(데이터)의 개수
 	static int[][] map;	//노드의 연결 정보 저장
 	static boolean[] visited;	//노드의 방문 여부 확인 
@@ -18,16 +18,16 @@ public class DfsMetrix {
 		
 		// 노드 간의 연결정보 저장
 		for(int i=0;i<edge;i++) {
-			int start=sc.nextInt();
-			int end=sc.nextInt();
+			int start=sc.nextInt();	//현재 노드
+			int end=sc.nextInt();	//연결된 노드
 			
 			map[start][end]=1;
-			map[end][start]=1;	// 한 방향으로 연결되는 경우 해당 코드 불필요
+			map[end][start]=1;	//한 방향으로 연결되는 경우 해당 코드 불필요
 		}
 		dfs(1);
 		sc.close();
 	}
-	// 재귀 함수를 이용해 구현
+	//재귀 함수를 이용해 구현
 	public static void dfs(int start) {
 		visited[start]=true;
 		System.out.print(start+" ");
