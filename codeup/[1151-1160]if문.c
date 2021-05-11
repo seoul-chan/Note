e619f16a082fd674292f5ac45b6a61c39b8129ee
seoul-chan <kkchan9210@gmail.com>
2021-05-11 18:00:41 +0900

    if문
    C언어 if문 연습

---
+++ codeup/[1151-1160]if문.c
@@ -0,0 +1,133 @@
+// 1160 : 아르바이트 가는 날
+#include <stdio.h>
+int main() {
+
+	int a;
+	scanf("%d", &a);
+	if (a == 1 || a == 3 || a == 5 || a == 7) {
+		printf("oh my god");
+	}
+	else {
+		printf("enjoy");
+	}
+}
+
+/*
+// 1159 : 특별한 공 던지기 3
+#include <stdio.h>
+int main() {
+	int a;
+	scanf("%d", &a);
+	if ((a>=50&&a<=70)||(a%6==0)) {
+		printf("win");
+	}
+	else {
+		printf("lose");
+	}
+}
+
+// 1158 : 특별한 공 던지기 2
+#include <stdio.h>
+int main() {
+	int a;
+	scanf("%d", &a);
+	if ((a>=30&&a<=40)||(a>=60&&a<=70)) {
+		printf("win");
+	}
+	else {
+		printf("lose");
+	}
+}
+
+// 1157 : 특별한 공 던지기 1
+#include <stdio.h>
+int main() {
+	float a;
+	scanf("%f", &a);
+	if (a>=50&&a<=60) {
+		printf("win");
+	}
+	else {
+		printf("lose");
+	}
+}
+
+// 1156 : 홀수 짝수 구별
+#include <stdio.h>
+int main() {
+	int a;
+	scanf("%d", &a);
+	if (a%2==0) {
+		printf("even");
+	}
+	else {
+		printf("odd");
+	}
+}
+
+// 1155 : 7의 배수
+#include <stdio.h>
+int main() {
+	int a;
+	scanf("%d", &a);
+	if (a%7==0) {
+		printf("multiple");
+	}
+	else {
+		printf("not multiple");
+	}
+}
+
+
+// 1151번 : 10보다 작은 수
+#include <stdio.h>
+int main() {
+	int a;
+	scanf("%d", &a);
+	if (a < 10) {
+		printf("small");
+	}
+}
+
+// 1152 : 10보다 작은 수 (else 버전)
+#include <stdio.h>
+int main() {
+	int a;
+	scanf("%d", &a);
+	if (a < 10) {
+		printf("small");
+	}
+	else {
+		printf("big");
+	}
+}
+
+// 1153 : 두 수의 대소 비교
+#include <stdio.h>
+int main() {
+	int a,b;
+	scanf("%d %d", &a,&b);
+	if (a > b) {
+		printf(">");
+	}
+	else if (a < b) {
+		printf("<");
+	}
+	else if (a == b) {
+		printf("=");
+	}
+}
+
+// 1154 : 큰수 - 작은수
+#include <stdio.h>
+int main() {
+	int a,b;
+	scanf("%d %d", &a,&b);
+	if (a >= b) {
+		printf("%d",a-b);
+	}
+	else if (a < b) {
+		printf("%d",b-a);
+	}
+}
+*/
